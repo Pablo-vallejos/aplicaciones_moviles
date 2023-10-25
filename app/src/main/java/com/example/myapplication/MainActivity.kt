efdb4b8078d5f79d5a19a874ed563b061c269f9e
Pablo-vallejos <pablovallejos94@gmail.com>
2023-10-24 22:16:31 -0300

    logica principal

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.widget.Button
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 
-//import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
    var player="p1"
@@ -23,6 +23,7 @@ class MainActivity : AppCompatActivity() {
         val b6 = findViewById<Button>(R.id.b6)
         val b7 = findViewById<Button>(R.id.b7)
         val b8 = findViewById<Button>(R.id.b8)
+        val btnReset = findViewById<Button>(R.id.btnReset)
 
         b1.setOnClickListener {
             buttonClick(b1)
@@ -60,6 +61,9 @@ class MainActivity : AppCompatActivity() {
             buttonClick(b9)
         }
 
+        btnReset.setOnClickListener {
+            reset()
+        }
 
     }
        fun buttonClick(btn:Button){
@@ -72,5 +76,87 @@ class MainActivity : AppCompatActivity() {
                     btn.text = "O"
                 }
             }
+           win()
+        }
+
+        fun win(){
+
+            // HORIZONTALES
+            if((b1.text == "X" && b2.text == "X" && b3.text == "X")
+                ||(b4.text == "X" && b5.text == "X" && b6.text == "X")
+                ||(b7.text == "X" && b8.text == "X" && b9.text == "X")
+                // DIAGONALES
+                ||(b1.text == "X" && b5.text == "X" && b9.text == "X")
+                ||(b7.text == "X" && b5.text == "X" && b3.text == "X")
+                //VERITACALES
+                ||(b1.text == "X" && b4.text == "X" && b7.text == "X")
+                ||(b2.text == "X" && b5.text == "X" && b8.text == "X")
+                ||(b3.text == "X" && b6.text == "X" && b9.text == "X")
+                ){
+                toast("X GANO")
+                disableButtons()
+                resultado.text="X GANO"
+            } else if (
+                (b1.text == "O" && b2.text == "O" && b3.text == "O")
+                ||(b4.text == "O" && b5.text == "O" && b6.text == "O")
+                ||(b7.text == "O" && b8.text == "O" && b9.text == "O")
+                // DIAGONALES
+                ||(b1.text == "O" && b5.text == "O" && b9.text == "O")
+                ||(b7.text == "O" && b5.text == "O" && b3.text == "O")
+                // VERTICALES
+                ||(b1.text == "O" && b4.text == "O" && b7.text == "O")
+                ||(b2.text == "O" && b5.text == "O" && b8.text == "O")
+                ||(b3.text == "O" && b6.text == "O" && b9.text == "O")
+            ){
+                toast("O GANO")
+                disableButtons()
+                resultado.text="O GANO"
+            } else {
+                if( b1.text != "" && b2.text != "" && b3.text != "" && b4.text != "" &&
+                    b5.text != "" && b6.text != "" && b7.text != "" && b8.text != "" &&
+                    b9.text != ""
+                    )
+                    toast("Empate")
+                    resultado.text="Empataron"
+
+            }
+        }
+
+
+        fun toast(msg:String){
+            Toast.makeText(this@MainActivity , msg,Toast.LENGHT_SHORT).show()
+        }
+
+        fun reset(){
+            b1.text = ""
+            b2.text = ""
+            b3.text = ""
+            b4.text = ""
+            b5.text = ""
+            b6.text = ""
+            b7.text = ""
+            b8.text = ""
+            b9.text = ""
+            b1.isEnabled = true
+            b2.isEnabled = true
+            b3.isEnabled = true
+            b4.isEnabled = true
+            b5.isEnabled = true
+            b6.isEnabled = true
+            b7.isEnabled = true
+            b8.isEnabled = true
+            b9.isEnabled = true
+        }
+
+        fun disableButtons(){
+            b1.isEnabled = false
+            b2.isEnabled = false
+            b3.isEnabled = false
+            b4.isEnabled = false
+            b5.isEnabled = false
+            b6.isEnabled = false
+            b7.isEnabled = false
+            b8.isEnabled = false
+            b9.isEnabled = false
         }
 }
